8da1a8ec3b3f83cc18e6d6705e5ae5b2ba260cd7
DariusJ268 <darius.juralevic@gmail.com>
2023-03-29 02:11:56 +0300

    kind of all

--- PHP/Puslapis/logic.php
+++ PHP/Puslapis/logic.php
@@ -1,18 +1,28 @@
 <?php
 session_start();
 
-
+$text = "ABCDEFGHIJKLMNOPRQSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890+-*/=-0#;/.>,<|(&^%$~!";
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-        $_SESSION["list"][] = ["item" => $_POST["item"] , "category" => $_POST["category"]];
-    }
+  if (strlen(preg_replace("/[^a-z]/i", "", $_POST["item"])) >=3 && strlen(preg_replace("/[^a-z]/i", "", $_POST["category"])) >=3) {
+    $_SESSION["list"][] = ["item" => $_POST["item"], "category" => $_POST["category"]];
+  }
+  header("Location: ./index.php");
+  die;
+}
 
+if (!isset($_SESSION["list"])) {
+  $_SESSION["list"] = [];
+}
 
-    print_r($_SESSION);
+// print_r($_SESSION);
 
-    echo "<br>";
+echo "<br>";
 
-    print_r($_POST);
+// print_r($_POST);
+?>
 
-
-  ?>
+<?php if (isset($_POST["item"]) && isset($_POST["category"])) { ?>
+  <h1><?= $_POST["item"] ?></h1>
+  <h1><?= $_POST["category"] ?></h1>
+<?php } ?>
