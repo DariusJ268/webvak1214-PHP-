b5446f137b8c9421141577892cc4eb42c8a7ced6
DariusJ268 <darius.juralevic@gmail.com>
2023-03-29 20:52:58 +0300

    final final final

--- PHP/Puslapis/index.php
+++ PHP/Puslapis/index.php
@@ -8,32 +8,37 @@
 </head>
 
 <body>
-    <div id = "top"></div>
+
+    <div id="top"></div>
     <h1>Pirkinių sąrašas</h1>
-    <form action="" method="post">
-        <input type="text" name="item" placeholder="Prekės pavadinimas">
-        <input type="text" name="category" placeholder="Prekės kategorija">
-        <button type="submit">Pridėti į sąrašą</button> <br>
-    </form>
+    <div class="container">
+        <form action="" method="post">
+            <input type="text" name="item" placeholder="Prekės pavadinimas">
+            <input type="text" name="category" placeholder="Prekės kategorija">
+            <button type="submit">Pridėti į sąrašą</button> <br>
+        </form>
 
+        <form action="" method="post">
+            <button type="submit" name="reset">reset</button>
+        </form>
+        <table class="table table-hover">
 
-    <table class="table table-hover">
-
-        <tr>
-            <div class="row">
-            <th class="col-1">Nr.</th>
-            <th class="col-5">Prekė</th>
-            <th class="col-6">Prekės kategorija</th>
-            </div>
-        </tr>
-        <?php for ($i = 0; $i < count($_SESSION["list"]); $i++) { ?>
             <tr>
-                <td><?= $i + 1 ?></td>
-                <td><?= $_SESSION["list"][$i]["item"] ?></td>
-                <td><?= $_SESSION["list"][$i]["category"] ?></td>
+                <div class="row">
+                    <th class="col-1">Nr.</th>
+                    <th class="col-5">Prekė</th>
+                    <th class="col-6">Prekės kategorija</th>
+                </div>
             </tr>
+            <?php for ($i = 0; $i < count($_SESSION["list"]); $i++) { ?>
+                <tr>
+                    <td><?= $i + 1 ?></td>
+                    <td><?= $_SESSION["list"][$i]["item"] ?></td>
+                    <td><?= $_SESSION["list"][$i]["category"] ?></td>
+                </tr>
 
-        <?php } ?>
+            <?php } ?>
+    </div>
 </body>
 
 </html>
--- PHP/Puslapis/logic.php
+++ PHP/Puslapis/logic.php
@@ -2,9 +2,11 @@
 session_start();
 
 $text = "ABCDEFGHIJKLMNOPRQSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890+-*/=-0#;/.>,<|(&^%$~!";
-
+if (isset($_POST["reset"])) {
+  $_SESSION['list'] = [];
+}
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-  if (strlen(preg_replace("/[^a-z]/i", "", $_POST["item"])) >=3 && strlen(preg_replace("/[^a-z]/i", "", $_POST["category"])) >=3) {
+  if (strlen(preg_replace("/[^a-z]/i", "", $_POST["item"])) >= 3 && strlen(preg_replace("/[^a-z]/i", "", $_POST["category"])) >= 3) {
     $_SESSION["list"][] = ["item" => $_POST["item"], "category" => $_POST["category"]];
   }
   header("Location: ./index.php");
@@ -15,14 +17,5 @@ if (!isset($_SESSION["list"])) {
   $_SESSION["list"] = [];
 }
 
-// print_r($_SESSION);
-
 echo "<br>";
-
-// print_r($_POST);
 ?>
-
-<?php if (isset($_POST["item"]) && isset($_POST["category"])) { ?>
-  <h1><?= $_POST["item"] ?></h1>
-  <h1><?= $_POST["category"] ?></h1>
-<?php } ?>
